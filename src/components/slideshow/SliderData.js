62ae5c79ae111d024096df5da111f670a0ec92ed
unknown <matej.pavic92@gmail.com>
2021-02-09 13:19:38 +0100

    removing unnecessary data

--- src/components/slideshow/SliderData.js
+++ src/components/slideshow/SliderData.js
@@ -2,60 +2,49 @@ export const SliderData = {
     top_row: [
         {
             id: 1,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-11.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-11.jpg'
         },
         {
             id: 2,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-7.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-7.jpg'
         },
         {
             id: 3,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-6.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-6.jpg'
         },
         {
             id: 4,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-9.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-9.jpg'
         },
         {
             id: 5,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-8.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-8.jpg'
         },
         {
             id: 6,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-5.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-5.jpg'
         }
     ],
     bottom_row: [
         {
             id: 7,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-1.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-1.jpg'
         },
         {
             id: 8,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-2.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-2.jpg'
         },
         {
             id: 9,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-3.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-3.jpg'
         },
         {
             id: 10,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-4.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-4.jpg'
         },
         {
             id: 11,
-            image: process.env.PUBLIC_URL + '/assets/slider-image-10.jpg',
-            dim: 200
+            image: process.env.PUBLIC_URL + '/assets/slider-image-10.jpg'
         },
     ]
 }
